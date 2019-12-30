4fe54c7810beb728ac81320d8bfe546b62c4fd57
SaltyAom <aomkirby@mystiar.com>
2019-12-30 14:57:38 +0700

    :fire: Add lifecycle hooks, file size reduced by 7.4 timegit add . (gZIP)

--- index.ts
+++ index.ts
@@ -1,20 +1,41 @@
-interface IProperty {
-    query: string
-    event: Event
-    then([state, set]:any): any
+        // "build": "rm -rf dist && tsc --build && terser dist/index.js -o dist/index.js --compress --mangle && cp project/package.json dist/package.json && cp README.md dist/README.md"
+
+
+interface MilkProperty {
+	query: string
+	event: string
+	then([state, set]: any): void
 }
 
-type TProperty = IProperty[]
-type Hooks = { type: string } | [any, Function] | {}
+type MilkEvent = MilkProperty[]
+interface Shadow {
+	shadow: "open" | "closed"
+}
+type Hooks = { type: string } | Shadow | [any, Function] | {}
 
-export const create = (execution: Function, hooks: Hooks = {}, ...properties: TProperty) =>
+interface UseProps {
+	type: "hookProp"
+}
+
+interface UseEffect {
+	type: "hookLifecycle"
+	callback(state: any, props: any): void
+	listener: string[]
+}
+
+export const create = (
+		execution: (display: Function, state: any, props: any) => null,
+		hooks: Hooks = {},
+		...properties: MilkEvent
+	) =>
 		class Milk extends HTMLElement {
 			props: any
 			state: any
 			setState: any
+			lifecycle: any
+			prevState: any
 
-			element: any
-			attachShadow: any
+			element: ShadowRoot
 
 			constructor() {
 				super()
@@ -22,11 +43,12 @@ export const create = (execution: Function, hooks: Hooks = {}, ...properties: TP
 				this.props = []
 				this.state = {}
 				this.setState = {}
+				this.lifecycle = {}
+				this.prevState = {}
 
 				this.element = this.attachShadow({ mode: "closed" })
 
-				this.initState()
-				this.update()
+				this.initialize()
 			}
 
 			static get observedAttributes() {
@@ -40,15 +62,26 @@ export const create = (execution: Function, hooks: Hooks = {}, ...properties: TP
 			}
 
 			attributeChangedCallback(name: string, oldValue: any, newValue: any) {
-				this.props[name] = newValue
+				let attrValue = this.parseAttribute(name)
+
+				this.props[name] = attrValue
 				this.update()
+
+				this.onPropsChange(name)
 			}
 
 			update() {
-				let mappedState = this.mapState()[0]
+				let mappedState = this.mapState()[0],
+					template = document.createElement("template")
 
 				execution(
-					(dom: string) => (this.element.innerHTML = this.reflectState(dom)),
+					(domString: string) => {
+						template.innerHTML = this.reflect(domString)
+						if (this.element.firstChild !== null)
+							while (this.element.firstChild) this.element.firstChild.remove()
+
+						this.element.appendChild(template.content.cloneNode(true))
+					},
 					mappedState,
 					this.props
 				)
@@ -56,24 +89,60 @@ export const create = (execution: Function, hooks: Hooks = {}, ...properties: TP
 				this.mapEvent()
 			}
 
-			initState() {
+			initialize() {
 				Object.entries(hooks).forEach(([name, hook]) => {
 					switch (hook.type) {
 						case "hookProp":
-							return (this.props[name] = this.getAttribute(name) || "")
+							let attrValue = this.parseAttribute(name)
+
+							return (this.props[name] = attrValue)
+
+						case "hookLifecycle":
+							return (this.lifecycle[name] = {
+								callback: hook.callback,
+								listener: hook.listener
+							})
 
 						default:
 							return ([this.state[name], this.setState[name]] = hook)
 					}
 				})
+				return this.update()
 			}
 
-			mapState() {
-				let mappedState = Object.assign({}, this.state)
+			onPropsChange(attributeName: string) {
+				let mappedState = this.mapState()[0]
 
-				Object.entries(this.state).map(
-					([name, hook]) => (mappedState[name] = this.state[name]())
+				Object.entries(this.lifecycle).forEach(
+					([lifeCycleName, { listener, callback }]: any) => {
+						if (listener.includes(attributeName))
+							callback(mappedState, this.props)
+					}
 				)
+			}
+
+			callStateLifeCycle(prevState: any, state: any) {
+				let diffState = Object.entries(state).filter(
+						([name, value]) => prevState[name] !== value
+					),
+					mappedDiff = diffState.map(([name, value]) => name)
+
+				Object.entries(this.lifecycle).forEach(
+					([lifeCycleName, { listener, callback }]: any) => {
+						mappedDiff.forEach(diffState => {
+							if (listener.includes(diffState)) callback(state, this.props)
+						})
+					}
+				)
+			}
+
+			mapState(state = this.state) {
+				let mappedState = Object.assign({}, state)
+
+				Object.entries(this.state).map(([name, hook]) => {
+					if (typeof state[name] === "function")
+						return (mappedState[name] = state[name]())
+				})
 
 				return [mappedState, this.setState]
 			}
@@ -84,14 +153,18 @@ export const create = (execution: Function, hooks: Hooks = {}, ...properties: TP
 				properties.map(property => {
 					let { query, event, then } = property
 					this.element.querySelector(query).addEventListener(event, () => {
+						let prevState = this.mapState(Object.assign({}, this.state))[0]
 						then(mappedState)
+						let state = this.mapState(Object.assign({}, this.state))[0]
+
+						this.callStateLifeCycle(prevState, state)
 						this.update()
 					})
 				})
 			}
 
-			reflectState(dom: string) {
-				let newDom = `${dom}`.replace(/{children}/, "<slot></slot>")
+			reflect(domString: string) {
+				let newDom = `${domString}`.replace(/{children}/, "<slot></slot>")
 
 				Object.entries(this.state).map(
 					([name, hook]) =>
@@ -111,8 +184,18 @@ export const create = (execution: Function, hooks: Hooks = {}, ...properties: TP
 
 				return newDom
 			}
+
+			parseAttribute(name: string) {
+				let attrValue: string | boolean = this.getAttribute(name) || ""
+
+				if (attrValue === "true") attrValue = true
+				else if (attrValue === "false") attrValue = false
+
+				return attrValue
+			}
 		},
-	define = (tagName: string, className: any) => customElements.define(tagName, className),
+	define = (tagName: string, className: any) =>
+		customElements.define(tagName, className),
 	useState = (initValue: any) => {
 		let _value = initValue,
 			getValue = () => _value,
@@ -120,15 +203,16 @@ export const create = (execution: Function, hooks: Hooks = {}, ...properties: TP
 
 		return [getValue, setValue]
 	},
-	useProps = () => {
+	useProps = (): UseProps => ({
+		type: "hookProp"
+	}),
+	useEffect = (
+		callback: (state: any, props: any) => void,
+		listener: string[]
+	): UseEffect => {
 		return {
-			type: "hookProp"
+			type: "hookLifecycle",
+			callback: callback,
+			listener: listener
 		}
 	}
-
-export default {
-	create: create,
-	define: define,
-	useState: useState,
-	useProps: useProps
-}
